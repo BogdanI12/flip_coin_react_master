8b4c4d9c7a50a09fcd86824da243cd84d66f1161
Bogdan Iovan <bogdan.iovan@visma.com>
2020-11-03 23:04:49 +0200

    Initial commit(coin spinning)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef,Suspense, Section } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+import "./App.scss"
+import { useGLTFLoader } from 'drei'
+ 
+const Model = () => {
+  const gltf = useGLTFLoader('/scene.gltf', true)
+  const coin = useRef(null);
+  useFrame(() => (coin.current.rotation.x = coin.current.rotation.x += 0.03));
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <mesh ref={coin}>
+        <primitive object={gltf.scene} dispose={null} args={[1,2]}/>
+      </mesh>
+  )
+}
+
+const Lights =() => {
+  return (
+    <>
+      <ambientLight intensity={1}/>
+      <directionalLight position={[10,10,5]} intensity={1}/>
+      <directionalLight position={[0,10,0]} intensity={1.5}/>
+    </>
+  )
+}
+
+function App(props) {
+  return <>
+    <Canvas colorManagement>
+      <Lights/>
+      <Suspense fallback={null}>
+          <Model/>
+      </Suspense>
+    </Canvas>
+  </>
 }
 
 export default App;
